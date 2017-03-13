3b3241bd2a3d903db56e2f0ea8ceb5e49d09aa47
Vitaliy <vetalsd2@gmail.com>
2017-03-13 17:52:33 +0200

    13.03

--- js/common.js
+++ js/common.js
@@ -1,51 +1,7 @@
-/*$(document).ready(function() {	
-	// placeholder
-	$('input').focus(function(){
-		$(this).data('placeholder',$(this).attr('placeholder'))
-		$(this).attr('placeholder','');
-	});
-	$('input').blur(function(){
-		$(this).attr('placeholder', $(this).data('placeholder'));
-	});
-	// fixed nav scroll
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 30){
-			$('header').css({
-				'height' : '40px',
-				'lineHeight' : '40px',
-				'backgroundColor' : '#95e1d3',
-				'opacity' : '.8'
-			});
-			$('header img').css({
-				'position' : 'relative',
-				'top' : '5px'
-			});
-		}
-		else {
-			$('header').css({
-				'height' : '80px',
-				'lineHeight' : '80px',
-				'backgroundColor' : 'transparent',
-				'opacity' : '1'
-			});
-			$('header img').css({
-				'position' : 'relative',
-				'top' : '0'
-			});
-		}
-	});
-	// accordion 
-	$('.service_content__accordion').on('click', '.service_content__pict_title', function() {
-		var accItem = $(this).closest('.service_content__wrapper').find('.service_content__pict_desc');
-		var arrow = $(this).find('.arrow');
-		if ( accItem.is(':hidden') ) {
-			$(this).closest('.service_content').find('.service_content__pict_desc').slideUp();
-			$(this).closest('.service_content').find('.arrow').attr('src', 'img/service_down.png');
-			accItem.slideDown();
-			arrow.attr('src', 'img/service_up.png');
-		}
-	});
+$(document).ready(function() {
+	$('.scrollto').mPageScroll2id();
 });
 $(window).load(function () {
-	$('.preloader').fadeIn('fast');
+	//$('.preloader_outer').delay(400).fadeIn('slow');
+	$('.preloader_outer').delay(2000).fadeOut('fast');
 });
